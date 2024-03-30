6fd93b7d1c1b90a315b9ebd08e151f03552713a1
髙野晃 <akira@akiranoMacBook-Pro.local>
2024-03-30 16:21:05 +0900

    DNSリバインディング攻撃の無効化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # DNSリバインディング攻撃の無効化
+  config.hosts.clear
 end
